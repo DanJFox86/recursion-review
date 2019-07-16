6ec58c9bd492264254bba311cfd9d968d6be08f8
DanJFox86 <fox.j.dan@gmail.com>
2019-07-15 17:49:45 -0700

    passes minimum tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,69 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  var output = "";
+  
+  // if (obj === function() {} || obj === undefined) {
+  //   return '{}';
+  // }
+
+  // '{"functions":null,"undefined":undefined}' to equal '{}'
+  
+//   unstringifiableValues = [
+//   {
+//     'functions': function() {},
+//     'undefined': undefined
+//   }
+// ];
+  
+  if ( (typeof obj === undefined || typeof obj === 'function' || typeof obj === 'symbol') || 
+        obj === NaN || obj === null || obj === Infinity ) {
+    // console.log("null");
+    return 'null';
+  }
+  
+  if(typeof obj === 'string') {
+    return "\"" + obj.toString() + "\"";
+  } else if(typeof obj === 'boolean' || typeof obj === 'number') {
+    return obj.toString();
+  }
+  
+
+  
+  if(Array.isArray(obj)) {
+    if (obj.length === 0 ) {
+      return '[]';
+    }
+    for (var i = 0; i < obj.length; i++) {
+      obj[i] = stringifyJSON(obj[i]);
+    }
+    output = "[" + obj.join(",") + "]";
+    return output;
+  } 
+  
+  else if(typeof obj === 'object') {
+    var keys = Object.keys(obj);
+    for(var i = 0; i < keys.length; i++) {
+      
+      if(typeof obj[keys[i]] !== 'function' && typeof obj[keys[i]] !== 'undefined') {
+        output += `${stringifyJSON(keys[i])}:${stringifyJSON(obj[keys[i]])}`;
+        if(i < keys.length - 1) {
+        output += ',';
+      }
+      }
+      
+      
+    }
+    output = `{${output}}`;
+    return output;
+    // output = "{" + output+ "}";
+
+  }
+  
+  // if primitive, return stringify primitive
+  // if obj is an array stringify [contents]
+  // 
+  //   
+  // Else if obj, stringify {contents} 
 };
